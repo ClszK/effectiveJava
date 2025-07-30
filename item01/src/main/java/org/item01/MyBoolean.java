5fc410c3a0f5525a7c52c7fc0c81f3c3867a694d
Clsz <45088680+ClszK@users.noreply.github.com>
2025-07-31 05:35:59 +0900

    feat: Green MyBoolean 캐싱 로직 추가

--- item01/src/main/java/org/item01/MyBoolean.java
+++ item01/src/main/java/org/item01/MyBoolean.java
@@ -5,10 +5,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MyBoolean {
 
+    private static final MyBoolean TRUE = new MyBoolean(true);
+    private static final MyBoolean FALSE = new MyBoolean(false);
+
     private final boolean value;
 
     public static MyBoolean valueOf(boolean b) {
-        return new MyBoolean(b);
+        return b ? TRUE : FALSE;
     }
 
     public boolean booleanValue() {
