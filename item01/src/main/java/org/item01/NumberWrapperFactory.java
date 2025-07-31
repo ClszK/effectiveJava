1a3865fd50bcbb652c635c6833be79dfa6752851
Clsz <45088680+ClszK@users.noreply.github.com>
2025-07-31 12:15:28 +0900

    refactor: NumberWrapperFactory refactoring
    - java 14 이후 도입된 switch expression으로 간결하게 변경
    - 예외 발생 시 명확한 메시지 전달

--- item01/src/main/java/org/item01/NumberWrapperFactory.java
+++ item01/src/main/java/org/item01/NumberWrapperFactory.java
@@ -3,10 +3,12 @@ package org.item01;
 public class NumberWrapperFactory {
 
     public static NumberWrapper of(Number n) {
-        if (n instanceof Integer)
-            return new IntegerWrapper(n.intValue());
-        else if (n instanceof Double)
-            return new DoubleWrapper(n.doubleValue());
-        throw new IllegalArgumentException();
+        return switch (n) {
+            case Integer i -> new IntegerWrapper(i);
+            case Double d -> new DoubleWrapper(d);
+            default ->
+                throw new IllegalArgumentException(
+                        "Unsupported type: " + n.getClass().getSimpleName());
+        };
     }
 }
