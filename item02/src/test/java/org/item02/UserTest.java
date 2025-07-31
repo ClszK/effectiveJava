90d4474309672753401ccc667f0c66e496bfd130
Clsz <45088680+ClszK@users.noreply.github.com>
2025-07-31 20:58:20 +0900

    test: Red 기본값 검증 추가

--- item02/src/test/java/org/item02/UserTest.java
+++ item02/src/test/java/org/item02/UserTest.java
@@ -1,5 +1,6 @@
 package org.item02;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -9,11 +10,25 @@ public class UserTest {
     @Test
     void builder_withRequiredOnly_buildsSuccessfully() {
         User user = User.builder()
-                        .username("alice")
-                        .email("alice@example.com")
-                        .build();
+                .username("alice")
+                .email("alice@example.com")
+                .build();
 
         assertEquals("alice", user.getUsername());
         assertEquals("alice@example.com", user.getEmail());
+    }
+
+    @Test
+    void builder_defaultOptionalValues() {
+        User user = User.builder()
+                .username("bob")
+                .email("bob@example.com")
+                .build();
+
+        assertAll("User 생성 테스트",
+                () -> assertEquals(0, user.getAge()),
+                () -> assertEquals("", user.getAddress()),
+                () -> assertFalse(user.isNewsletterOptIn()));
+
     }
 }
