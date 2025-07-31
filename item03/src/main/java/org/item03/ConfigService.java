4c47318b3e1a5a6d46bf3cde8cf3d0684a6854de
Clsz <45088680+ClszK@users.noreply.github.com>
2025-08-01 03:13:37 +0900

    feat: Green ConfigService 테스트 통과

--- item03/src/main/java/org/item03/ConfigService.java
+++ item03/src/main/java/org/item03/ConfigService.java
@@ -1,9 +1,20 @@
 package org.item03;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConfigService {
+
     private static final ConfigService INSTANCE = new ConfigService();
+    private final Map<String, String> configs = new HashMap<>();
+
+    private ConfigService() { }
 
     public static ConfigService getInstance() {
         return INSTANCE;
+    }
+
+    public String getProperty(String key) {
+        return configs.getOrDefault(key, "");
     }
 }
