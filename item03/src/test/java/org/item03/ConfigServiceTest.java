3fce542509e0aecc8e5309d79d7d8141cc02b329
Clsz <45088680+ClszK@users.noreply.github.com>
2025-08-01 03:10:47 +0900

    test: Red ConfigService 테스트 추가
    - 기본 속성값은 비어있다?

--- item03/src/test/java/org/item03/ConfigServiceTest.java
+++ item03/src/test/java/org/item03/ConfigServiceTest.java
@@ -1,5 +1,6 @@
 package org.item03;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 import org.junit.jupiter.api.Test;
@@ -11,5 +12,11 @@ public class ConfigServiceTest {
         ConfigService first = ConfigService.getInstance();
         ConfigService second = ConfigService.getInstance();
         assertSame(first, second);
+    }
+
+    @Test
+    void config_defaultPropertyIsEmpty() {
+        ConfigService cfs = ConfigService.getInstance();
+        assertEquals("", cfs.getProperty("anyKey"));
     }
 }
