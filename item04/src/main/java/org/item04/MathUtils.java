9079380bf0847ebeb667a9eb00d0a7a14697ad65
Clsz <45088680+ClszK@users.noreply.github.com>
2025-08-01 21:21:44 +0900

    test: Red MathUtils 소수 판별 테스트 추가

---
+++ item04/src/main/java/org/item04/MathUtils.java
@@ -0,0 +1,5 @@
+package org.item04;
+
+public class MathUtils {
+
+}
---
+++ item04/src/test/java/org/item04/MathUtilsTest.java
@@ -0,0 +1,72 @@
+package org.item04;
+
+import org.junit.jupiter.api.Test;
+
+public class MathUtilsTest {
+
+    @Test
+    void isPrime_with_prime_numbers() {
+        assertTrue(MathUtils.isPrime(2));
+        assertTrue(MathUtils.isPrime(13));
+        assertTrue(MathUtils.isPrime(100000000003));
+        assertTrue(MathUtils.isPrime(100000000019));
+        assertTrue(MathUtils.isPrime(100000000057));
+        assertTrue(MathUtils.isPrime(100000000069));
+        assertTrue(MathUtils.isPrime(100000000081));
+        assertTrue(MathUtils.isPrime(100000000123));
+        assertTrue(MathUtils.isPrime(100000000151));
+        assertTrue(MathUtils.isPrime(100000000169));
+        assertTrue(MathUtils.isPrime(100000000183));
+        assertTrue(MathUtils.isPrime(100000000211));
+        assertTrue(MathUtils.isPrime(100000000237));
+        assertTrue(MathUtils.isPrime(100000000267));
+        assertTrue(MathUtils.isPrime(100000000273));
+        assertTrue(MathUtils.isPrime(100000000297));
+        assertTrue(MathUtils.isPrime(100000000307));
+        assertTrue(MathUtils.isPrime(100000000321));
+        assertTrue(MathUtils.isPrime(100000000333));
+        assertTrue(MathUtils.isPrime(100000000351));
+        assertTrue(MathUtils.isPrime(100000000369));
+        assertTrue(MathUtils.isPrime(100000000391));
+        assertTrue(MathUtils.isPrime(100000000399));
+        assertTrue(MathUtils.isPrime(100000000409));
+        assertTrue(MathUtils.isPrime(100000000423));
+        assertTrue(MathUtils.isPrime(100000000441));
+        assertTrue(MathUtils.isPrime(100000000453));
+        assertTrue(MathUtils.isPrime(100000000471));
+        assertTrue(MathUtils.isPrime(100000000489));
+        assertTrue(MathUtils.isPrime(100000000501));
+        assertTrue(MathUtils.isPrime(100000000513));
+        assertTrue(MathUtils.isPrime(100000000537));
+        assertTrue(MathUtils.isPrime(12345678919));
+        assertTrue(MathUtils.isPrime(12345678929));
+        assertTrue(MathUtils.isPrime(12345678941));
+        assertTrue(MathUtils.isPrime(12345678943));
+        assertTrue(MathUtils.isPrime(12345678947));
+        assertTrue(MathUtils.isPrime(12345678953));
+        assertTrue(MathUtils.isPrime(12345678959));
+        assertTrue(MathUtils.isPrime(12345678961));
+        assertTrue(MathUtils.isPrime(12345678967));
+        assertTrue(MathUtils.isPrime(12345678971));
+        assertTrue(MathUtils.isPrime(12345678973));
+        assertTrue(MathUtils.isPrime(12345678977));
+        assertTrue(MathUtils.isPrime(12345678979));
+        assertTrue(MathUtils.isPrime(12345678983));
+        assertTrue(MathUtils.isPrime(12345678989));
+        assertTrue(MathUtils.isPrime(12345678997));
+        assertTrue(MathUtils.isPrime(12345679003));
+        assertTrue(MathUtils.isPrime(12345679009));
+        assertTrue(MathUtils.isPrime(12345679013));
+        assertTrue(MathUtils.isPrime(12345679021));
+        assertTrue(MathUtils.isPrime(12345679027));
+        assertTrue(MathUtils.isPrime(12345679033));
+        assertTrue(MathUtils.isPrime(12345679039));
+        assertTrue(MathUtils.isPrime(12345679043));
+        assertTrue(MathUtils.isPrime(12345679049));
+        assertTrue(MathUtils.isPrime(12345679051));
+        assertTrue(MathUtils.isPrime(12345679057));
+        assertTrue(MathUtils.isPrime(12345679061));
+        assertTrue(MathUtils.isPrime(12345679067));
+        assertTrue(MathUtils.isPrime(12345679073));
+    }
+}
