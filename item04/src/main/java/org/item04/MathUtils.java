bd58da55b02f28fae7c47903cc21605724c494b9
Clsz <45088680+ClszK@users.noreply.github.com>
2025-08-01 22:55:30 +0900

    refactor: MathUtils 고도화
    - final 선언으로 상속 불가

--- item04/src/main/java/org/item04/MathUtils.java
+++ item04/src/main/java/org/item04/MathUtils.java
@@ -1,6 +1,17 @@
 package org.item04;
 
-public class MathUtils {
+/**
+ * Math 유틸리티 클래스
+* <p>
+* 기본 생성자 private
+* final 선언으로 상속 불가
+* </p>
+* <ul>
+*   <li>isPrime() : 현재 소수인지 아닌지 판별</li>
+* </ul>
+*
+*/
+public final class MathUtils {
 
     private MathUtils() { }
 
