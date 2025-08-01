a5021abcdfd89694cf068d06d34a118ce5ef4d09
Clsz <45088680+ClszK@users.noreply.github.com>
2025-08-02 00:56:05 +0900

    test: Red processPayment 매개변수 음수 테스트 코드 추가

--- item05/src/test/java/org/item05/PaymentServiceTest.java
+++ item05/src/test/java/org/item05/PaymentServiceTest.java
@@ -1,14 +1,17 @@
 package org.item05;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import lombok.Getter;
 
 /**
-* 결제 처리(PaymentServcie) 시나리오, 외부 결제 게이트웨이(PaymentGateway)를 생성자 주입
-*/
+ * 결제 처리(PaymentServcie) 시나리오, 외부 결제 게이트웨이(PaymentGateway)를 생성자 주입
+ */
 class PaymentServiceTest {
     @Test
     void processPayment_withValidAmountInvokesGateway() {
@@ -19,6 +22,16 @@ class PaymentServiceTest {
 
         assertEquals(5_000, fakeGateway.getLastChargedAmount());
         assertEquals(1, fakeGateway.getInvokeCount());
+    }
+
+    @ParameterizedTest(name = "[{index}] amount = {0}")
+    @ValueSource(ints = { 0, -1_000 })
+    void processPayment_invalidAmount_throwsIllegalArgumentException(int invalidAmount) {
+        var fakeGateway = new FakePaymentGateway();
+        var service = new PaymentService(fakeGateway);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> service.processPayment(invalidAmount));
     }
 
     @Getter
