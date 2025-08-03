e4877697cfab3908c1c922461ec5d3c6c72ca94d
Clsz <45088680+ClszK@users.noreply.github.com>
2025-08-04 07:15:52 +0900

    test: Red 메모리 누수 발생 확인 테스트 추가

--- item07/src/test/java/org/item07/StackTest.java
+++ item07/src/test/java/org/item07/StackTest.java
@@ -1,6 +1,9 @@
 package org.item07;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.lang.reflect.Field;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +17,21 @@ class StackTest {
 
         assertEquals("B", stack.pop());
         assertEquals("A", stack.pop());
+    }
+
+    @Test
+    void pop_should_null_out_obsolete_reference() throws Exception {
+        Stack<String> stack = new Stack<>();
+        stack.push("A");
+        stack.push("B");
+
+        String poped = stack.pop();
+        assertEquals("B", poped);
+
+        Field elementsField = Stack.class.getDeclaredField("elements");
+        elementsField.setAccessible(true);
+        Object[] elems = (Object[]) elementsField.get(stack);
+
+        assertNull(elems[1], "pop 후 obsolete 참조를 null 처리해야 한다");
     }
 }
