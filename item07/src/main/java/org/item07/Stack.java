96420a1d19ddca14c6862f0672d1f491caf04a57
Clsz <45088680+ClszK@users.noreply.github.com>
2025-08-04 07:05:09 +0900

    feat: Green LIFO 기능 구현

---
+++ item07/src/main/java/org/item07/Stack.java
@@ -0,0 +1,16 @@
+package org.item07;
+
+public class Stack<E> {
+    private final Object[] elements = new Object[10];
+    private int size = 0;
+
+    public void push(E e) {
+        elements[size++] = e;
+    }
+
+    @SuppressWarnings("unchecked")
+    public E pop() {
+        return (E) elements[--size];
+    }
+
+}
