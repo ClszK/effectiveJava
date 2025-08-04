4923493e93045a174184178b3ef82a0460efb9e1
Clsz <45088680+ClszK@users.noreply.github.com>
2025-08-04 10:16:42 +0900

    feat: Green Capacity 자동 확장 및 validation 구현

--- item07/src/main/java/org/item07/Stack.java
+++ item07/src/main/java/org/item07/Stack.java
@@ -1,18 +1,26 @@
 package org.item07;
 
+import java.util.Arrays;
+
 public class Stack<E> {
-    private final Object[] elements = new Object[10];
+    private Object[] elements = new Object[10];
     private int size = 0;
 
     public void push(E e) {
+        if (size == elements.length) {
+            int newCap = elements.length * 2;
+            elements = Arrays.copyOf(elements, newCap);
+        }
         elements[size++] = e;
     }
 
     @SuppressWarnings("unchecked")
     public E pop() {
-        Object elem = elements[--size];
-        elements[size] = null;
+        int idx = --size;
+        if (idx < 0)
+            throw new IllegalArgumentException("stack size is 0");
+        Object elem = elements[idx];
+        elements[idx] = null;
         return (E) elem;
     }
-
 }
